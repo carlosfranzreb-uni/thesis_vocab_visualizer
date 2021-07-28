03ac53e14b192bbba37f2758becdb7d67a7cee78
Carlos Franzreb <carlos.franzreb@fokus.fraunhofer.de>
2021-07-28 14:18:43 +0200

    Highlight words that are contained in the vocabulary.

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,5 @@
+
+import json
 from flask import render_template
 
 from app import app, utils
@@ -9,10 +11,15 @@ def home():
     form = MainForm()
     title, abstract = '', ''
     if form.is_submitted():
-        title, abstract = utils.get_doc(form.id.data)
+      title, abstract = utils.get_doc(form.id.data)
+      vocab = json.load(open('app/static/data/vocab/repo_vocab.json'))
+      title_words = utils.get_vocab_words(title, vocab)
+      title_marked = utils.format_text(title_words)
+      abstract_words = utils.get_vocab_words(abstract, vocab)
+      abstract_marked = utils.format_text(abstract_words)
     return render_template(
       'main.html',
-      title=title,
-      abstract=abstract,
+      title=title_marked,
+      abstract=abstract_marked,
       form=form
     )
--- app/utils.py
+++ app/utils.py
@@ -5,8 +5,46 @@ import os
 
 def get_doc(id):
   """ Return the title and abstract of the doc with the given DOI. """
-  data = json.load(open(f'app/static/data/json/dim/all/relevant_data.json'))
+  data = json.load(open('app/static/data/json/dim/all/relevant_data.json'))
   if id not in data:
     return 'Document not part of this work.'
   return data[id].values()
-  
+
+
+def get_vocab_words(text, vocab):
+  """ Given a text, return a dictionary where each word of the text is a key
+  and its value is a boolean indicating if the word is in the vocabulary. 
+  Check also for n-grams up to length 4. Each a word is part of an n-gram, 
+  output it only there and not individually. """
+  res = dict()
+  words = text.split(' ')
+  i = 0
+  while i < len(words):
+    for j in range(i+4, i, -1):
+      ngram = ' '.join(words[i:j])
+      if ngram in vocab:
+        res[ngram] = True
+        i = j
+        break
+      elif j == i + 1:
+        res[ngram] = False
+        i += 1
+  return res
+
+
+def format_text(words):
+  """ Given a dict with words as returned by get_vocab_words(), output a string
+  that encloses each word whose value is True with the <mark> tags. """
+  res = ""
+  for word in words:
+    if words[word] is True:
+      res += f'<mark>{word}</mark> '
+    else:
+      res += word + ' '
+  return res[:-1]
+
+
+if __name__ == '__main__':
+  vocab = json.load(open('app/static/data/vocab/repo_vocab.json'))
+  words = get_vocab_words('this is a text with singular value decomposition and visual cortex', vocab)
+  print(words)
