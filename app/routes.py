a25cc9570fb61bdebedfeb0c74605fc300349509
Carlos Franzreb <carlos.franzreb@fokus.fraunhofer.de>
2021-09-07 15:59:12 +0200

    Use new file for data.

--- app/routes.py
+++ app/routes.py
@@ -8,13 +8,18 @@ from app.forms import MainForm
 
 @app.route("/", methods=['GET','POST'])
 def home():
-    form = MainForm()
-    title_marked, abstract_marked = 'Title', 'Abstract'
-    if form.is_submitted():
+  form = MainForm()
+  title_marked, abstract_marked = 'Title', 'Abstract'
+  if form.is_submitted():
+    doc = utils.get_doc(form.id.data)
     vocab = utils.get_vocab(form.vocab.data)
-    return render_template(
-      'main.html',
-      title=title_marked,
-      abstract=abstract_marked,
-      form=form
-    )
+    title_words = utils.get_vocab_words(doc['title']['processed'], vocab)
+    title_marked = utils.format_text(doc['title']['original'], title_words)
+    abstract_words = utils.get_vocab_words(doc['abstract']['processed'], vocab)
+    abstract_marked = utils.format_text(doc['abstract']['original'], abstract_words)
+  return render_template(
+    'main.html',
+    title=title_marked,
+    abstract=abstract_marked,
+    form=form
+  )
--- app/utils.py
+++ app/utils.py
@@ -16,40 +16,59 @@ def get_vocab(vocab_name):
 
 def get_doc(id):
   """ Return the title and abstract of the doc with the given DOI. """
-  data = json.load(open('app/static/data/json/dim/all/relevant_data.json'))
+  data = json.load(open('app/static/data/json/dim/all/data_processed.json'))
+  original_data = json.load(open('app/static/data/json/dim/all/relevant_data.json'))
   if id not in data:
     return 'Document not part of this work.'
-  return data[id].values()
+  return {
+    'title': {
+      'original': original_data[id]['title'], 'processed': data[id]['title']
+    },
+    'abstract': {
+      'original': original_data[id]['abstract'], 'processed': data[id]['abstract']
+    }
+  }
 
 
-def get_vocab_words(text, vocab):
-  """ Given a text, return a dictionary where each word of the text is a key
-  and its value is a boolean indicating if the word is in the vocabulary. 
-  Check also for n-grams up to length 4. Each a word is part of an n-gram, 
-  output it only there and not individually. """
-  res = dict()
-  words = text.split(' ')
+def get_vocab_words(processed, vocab):
+  """ Given a text, return a list of tuples where each tuple contains the 
+  indexes of the words that compose that ngram, if it is present in the
+  vocabulary. Check ngrams up to length 4. """
+  res = []
   i = 0
-  while i < len(words):
+  while i < len(processed):
     for j in range(i+4, i, -1):
-      ngram = ' '.join(words[i:j])
+      ngram = ' '.join(processed[i:j])
       if ngram in vocab:
-        res[ngram] = True
+        res.append(range(i, j))
         i = j
         break
       elif j == i + 1:
-        res[ngram] = False
         i += 1
   return res
 
 
-def format_text(words):
-  """ Given a dict with words as returned by get_vocab_words(), output a string
-  that encloses each word whose value is True with the <mark> tags. """
+def format_text(text, indexes):
+  """ Given a text and the indexes returned by get_vocab_words(), output a
+  string that encloses each ngram present in 'indexes' with the <mark> tags. """
+  if len(indexes) == 0:
+    return text
   res = ""
-  for word in words:
-    if words[word] is True:
-      res += f'<mark>{word}</mark> '
+  ngram = indexes.pop(0)
+  for idx, word in enumerate(text.split(' ')):
+    if idx in ngram:
+      word_inserted = False
+      if ngram[0] == idx:
+        res += f'<mark>{word} '
+        word_inserted = True
+      if ngram[-1] == idx:
+        if not word_inserted:
+          res += word
+          word_inserted = True
+        res += '</mark> '
+        ngram = indexes.pop(0)
+      if not word_inserted:
+        res += word + ' '
     else:
       res += word + ' '
   return res[:-1]
